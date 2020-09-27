028470968953312bb8990a29135e34a2c7be2678
Monica Rivera <mriverap@outlook.com>
2020-09-27 00:43:20 -0700

    Add solution

---
+++ AdaptiveBotsProjectTemplates/obj/Release/ProjectTemplates/EmptyBotAdaptive/Dialogs/RootDialog.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using Microsoft.Bot.Builder.Dialogs.Adaptive;
+using Microsoft.Bot.Builder.Dialogs.Adaptive.Actions;
+using Microsoft.Bot.Builder.Dialogs.Adaptive.Conditions;
+
+namespace $safeprojectname$.Dialogs
+{
+    public class RootDialog : AdaptiveDialog
+    {
+        public RootDialog() : base(nameof(RootDialog))
+        {
+            Triggers = new List<OnCondition>
+            {
+                new OnUnknownIntent
+                {
+                    Actions =
+                    {
+                        new SendActivity("Hi, we are up and running!!"),
+                    }
+                },
+            };
+        }
+    }
+}
---
+++ AdaptiveBotsProjectTemplates/obj/Release/ProjectTemplates/EmptyBotAdaptive/Startup.cs
@@ -0,0 +1,99 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+//
+// Generated with Bot Builder V4 SDK Template for Visual Studio EmptyBot v4.10.3
+
+using $safeprojectname$.Dialogs;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Builder.AI.Luis;
+using Microsoft.Bot.Builder.AI.QnA;
+using Microsoft.Bot.Builder.BotFramework;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Builder.Dialogs.Adaptive;
+using Microsoft.Bot.Builder.Dialogs.Declarative;
+using Microsoft.Bot.Builder.Integration.AspNet.Core;
+using Microsoft.Bot.Connector.Authentication;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace $safeprojectname$
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers().AddNewtonsoftJson();
+
+            // Create the Bot Framework Adapter with error handling enabled.
+            services.AddSingleton<IBotFrameworkHttpAdapter, AdapterWithErrorHandler>();
+
+            // Create the bot. the ASP Controller is expecting an IBot.
+            services.AddSingleton<IBot, DialogBot<RootDialog>>();
+
+            // The adaptive dialog that will be run by the bot.
+            services.AddSingleton<RootDialog>();
+
+            // Components common to all adaptive dialogs.
+            ComponentRegistration.Add(new AdaptiveComponentRegistration());
+
+            // Common memory scopes and path resolvers.
+            ComponentRegistration.Add(new DialogsComponentRegistration());
+
+            // Components used to consume declarative dialogs.
+            ComponentRegistration.Add(new DeclarativeComponentRegistration());
+
+            // Components used for language generation features.
+            ComponentRegistration.Add(new LanguageGenerationComponentRegistration());
+
+            // Components used for LUIS (language understanding) features.
+            ComponentRegistration.Add(new LuisComponentRegistration());
+
+            // Components used for QnA Maker (language understanding) features.
+            ComponentRegistration.Add(new QnAMakerComponentRegistration());
+
+            // Create the credential provider to be used with the Bot Framework Adapter.
+            services.AddSingleton<ICredentialProvider, ConfigurationCredentialProvider>();
+
+            // Create the storage we'll be using for User and Conversation state. (Memory is great for testing purposes.) 
+            services.AddSingleton<IStorage, MemoryStorage>();
+
+            // Create the User state. (Used in this bot's Dialog implementation.)
+            services.AddSingleton<UserState>();
+
+            // Create the Conversation state. (Used by the Dialog system itself.)
+            services.AddSingleton<ConversationState>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseDefaultFiles()
+                .UseStaticFiles()
+                .UseWebSockets()
+                .UseRouting()
+                .UseAuthorization()
+                .UseEndpoints(endpoints =>
+                {
+                    endpoints.MapControllers();
+                });
+
+            // app.UseHttpsRedirection();
+        }
+    }
+}
